867eaee5405de750c290acadd44ed55730933da6
kim mi seon <miseon920@gmail.com>
2023-04-13 17:32:28 +0900

    토글리팩토링해결

--- vue-todo-vuex/src/store/store.js
+++ vue-todo-vuex/src/store/store.js
@@ -8,25 +8,23 @@ Vue.use(Vuex);
 let sortItem = [];
 
 function sortlist(state) {
-    const trueItem = state.filter(
-        (item) => item.completed === true
-    );
-    const falseItem = state.filter(
-        (item) => item.completed === false
-    );
+    const trueItem = state
+        .filter((item) => item.completed === true)
+        .sort(function (a, b) {
+            return b.value.time - a.value.time; //내림차순
+        });
+    const falseItem = state
+        .filter((item) => item.completed === false)
+        .sort(function (a, b) {
+            return b.value.time - a.value.time; //내림차순
+        });
 
-    trueItem.sort(function (a, b) {
-        return b.value.time - a.value.time; //내림차순
-    });
-
-    falseItem.sort(function (a, b) {
-        return b.value.time - a.value.time; //내림차순
-    });
     sortItem = [...falseItem, ...trueItem];
-    return  sortItem;
+    return sortItem;
 }
 const storage = {
-    fetch() { //네트워크의 리소스를 쉽게 비동기적으로 가져오기
+    fetch() {
+        //네트워크의 리소스를 쉽게 비동기적으로 가져오기
         //app에 있는 created를 store에 붙이기
         let arr = [];
         if (localStorage.length > 0) {
@@ -34,7 +32,8 @@ const storage = {
                 if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
                     const item = localStorage.getItem(localStorage.key(i));
                     arr.push(JSON.parse(item)); //arr배열을 만들어서 푸시해주기
-                    sortlist(arr);  
+                    console.log(arr);
+                    sortlist(arr);
                 }
             }
         }
@@ -51,27 +50,33 @@ export const store = new Vuex.Store({
         todoItems: storage.fetch(), //위에 만들어준 값을 넣어준다
     },
     getters: {
-        getTotalNum(state) { //인자는 state
+        getTotalNum(state) {
+            //인자는 state
             return state.todoItems.length;
         },
-        getTrueNum(state) { //인자는 state
+        getTrueNum(state) {
+            //인자는 state
             const trueItem = state.todoItems.filter(
                 (item) => item.completed === true
             );
             return trueItem.length;
         },
     },
-    mutations:{
-        addOneItem(state, value) { 
+    mutations: {
+        addOneItem(state, value) {
             //console.log(value)
             const obj = { completed: false, value: value }; //const를 쓰면 재선언할 수 없어 디버깅할 때 충돌을 줄일 수 있음
             //오브젝트로 만들어줬기 때문에 obj를 찍게 되면 object object가 찍힘, 따라서 문자열로 변환해 주는것
             localStorage.setItem(value.item, JSON.stringify(obj)); //JavaScript 값이나 객체를 JSON 문자열로 변환
-            if(!state.todoItems.some(element => element.value.item == value.item)){
+            if (
+                !state.todoItems.some(
+                    (element) => element.value.item == value.item
+                )
+            ) {
                 state.todoItems.unshift(obj);
-            }else{
-                alert('이미 작성된 내용입니다.');
-            }           
+            } else {
+                alert("이미 작성된 내용입니다.");
+            }
         },
         removeOneItem(state, payload) {
             //console.log(payload);
@@ -80,11 +85,17 @@ export const store = new Vuex.Store({
         },
         toggleOneItem(state, payload) {
             //todoItem.completed = !todoItem.completed; //이벤트로 넘어온값을 바꾸는 것을 안티패턴이라고 하며 어짜피 props로 내려준 값이 변경되므로 아래와같이 변경한것
-            state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed;
+            state.todoItems[payload.index].completed =
+                !state.todoItems[payload.index].completed;
             //localStorage 갱신하기
             localStorage.removeItem(payload.todoItem.value.item); //localStorage는 업데이트 기능이 없으므로 삭제 후
-            localStorage.setItem(payload.todoItem.value.item, JSON.stringify(payload.todoItem)); //새로 추가해준다, 이때 completed 값이 바꼈으므로 바뀐값이 들어오게 된다.
-            sortlist(state.todoItems); //리팩토링이안돼네...추후에 해결할것 ㅠㅠ
+            localStorage.setItem(
+                payload.todoItem.value.item,
+                JSON.stringify(payload.todoItem)
+            ); //새로 추가해준다, 이때 completed 값이 바꼈으므로 바뀐값이 들어오게 된다.
+            
+            const finalData = JSON.parse(JSON.stringify(state.todoItems));
+            return state.todoItems = sortlist(finalData);
         },
         clearAllItems(state) {
             localStorage.clear();
